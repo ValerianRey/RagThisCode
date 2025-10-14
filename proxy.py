93c84874566ae3e1cba71cbedfea0b0920ee04d2
Matthieu Buot de l'Epine <matthieu2301@hotmail.fr>
2025-10-14 18:34:56 +0200

    bind to all interfaces

--- proxy.py
+++ proxy.py
@@ -186,4 +186,4 @@ async def chat_stream(body: ChatRequest) -> StreamingResponse:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app=app, host="127.0.0.1", port=7070)
+    uvicorn.run(app=app, host="0.0.0.0", port=7070)
